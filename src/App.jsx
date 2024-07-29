22655f54757145eb5e3cfa9a78985b145df0ea98
Alireza Aberoumand <alirezaaberoumand7192@gmail.com>
2024-07-28 18:35:45 -0700

    Project is done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import React from 'react'
+
+function App() {
+  return (
+    <div className="body">
+      <div className="app-container">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
